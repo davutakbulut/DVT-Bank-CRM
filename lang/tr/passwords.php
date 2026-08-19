d6e0932d29707669003ffc4c101ca069d3a4d60e
Davut Akbulut <davutakbulut@Davut-MacBook-Pro.local>
2026-08-19 18:48:04 +0300

    [FAZ-3][BUGFIX] Add Turkish passwords.php translation file for password reset rate limiting messages

---
+++ lang/tr/passwords.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Şifre Sıfırlama Türkçe Metinleri
+    |--------------------------------------------------------------------------
+    */
+
+    'reset' => 'Şifreniz başarıyla sıfırlandı!',
+    'sent' => 'Şifre sıfırlama bağlantınız e-posta adresinize gönderildi!',
+    'throttled' => 'Çok fazla istek gönderdiniz. Lütfen tekrar denemeden önce bir süre bekleyin.',
+    'token' => 'Bu şifre sıfırlama bağlantısı geçersiz veya süresi dolmuş.',
+    'user' => 'Bu e-posta adresiyle kayıtlı bir kullanıcı bulunamadı.',
+
+];
